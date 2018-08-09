0193c141f284d6af3009512abe09ffe3bdc13644
Verkuijlen Thijs <thijs.verkuijlen@capgemini.com>
2018-08-09 12:41:15 +0000

    Added voice control

--- ui/webapp/controller/Worklist.controller.js
+++ ui/webapp/controller/Worklist.controller.js
@@ -35,7 +35,7 @@ sap.ui.define([
 			this.aEntities = ["Sectors", "EmergingTechnologies", "Processes", "SAPProducts"];
 			oModel.metadataLoaded().then(this.readNumberOfElements());
 		},
-		
+
 		readNumberOfElements: function () {
 			var aRandomMax = [],
 				self = this,
@@ -71,7 +71,7 @@ sap.ui.define([
 					self.getOwnerComponent().getModel().read("/" + Object.keys(element)[0] + "(" + rn + ")", {
 						success: function (oData, response) {
 							processed++;
-							randomObject[Object.keys(element)[0]] = oData.desciption;
+							randomObject[Object.keys(element)[0]] = oData.description;
 							if (processed === randomMaxModel.length) {
 								var randomModel = new JSONModel(randomObject);
 								self.getOwnerComponent().setModel(randomModel, "random");
@@ -98,6 +98,41 @@ sap.ui.define([
 				this.getView().addDependent(this._oPopover);
 			}
 			this._oPopover.openBy(oEvent.getSource());
+		},
+
+		onMicPress: function (oEvent) {
+			var self = this,
+				oMicButton = oEvent.getSource();
+			oMicButton.setEnabled(false)
+			if ('webkitSpeechRecognition' in window) {
+				var speechRecognizer = new webkitSpeechRecognition();
+				speechRecognizer.continuous = true;
+				speechRecognizer.interimResults = false;
+				speechRecognizer.lang = 'en-US';
+				speechRecognizer.start();
+
+				jQuery.sap.delayedCall(4000, this, function () { // this has to be implemented so as the control comes back after 5 seconds
+					speechRecognizer.stop();
+					oMicButton.setEnabled(true);
+				});
+
+				speechRecognizer.onresult = function (event) {
+					var last = event.results.length - 1;
+					var text = event.results[last][0].transcript;
+
+					sap.m.MessageToast.show(text + ". Confidence: " + event.results[0][0].confidence);
+					self._chechInput(text);
+				};
+				speechRecognizer.onerror = function (event) {
+					oMicButton.setEnabled(true);
+				};
+			}
+		},
+
+		_chechInput: function (text) {
+			if (text.includes("randomise")) {
+				this.readNumberOfElements();
+			}
 		}
 
 	});
